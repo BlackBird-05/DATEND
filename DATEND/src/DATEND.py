a484afcd4db2f6db0e2571d98bb6cdd9bf09bad0
Rtong <73419916+BlackBird-05@users.noreply.github.com>
2026-01-17 15:53:31 +0800

    Made changes to datend.py

--- DATEND/src/DATEND.py
+++ DATEND/src/DATEND.py
@@ -1,5 +1,21 @@
+import subprocess
+try:
+    import PySide6
+except:
+    try:
+        subprocess.check_call(["pip", "install", 'PySide6'])
+    except:
+        subprocess.check_call(["pip3", "install", 'PySide6'])
+    import PySide6
 
-
+try:
+    import docx
+except:
+    try:
+        subprocess.check_call(["pip", "install", 'python-docx'])
+    except:
+        subprocess.check_call(["pip3", "install", 'python-docx'])
+    import docx
 
 import subprocess
 import tkinter
